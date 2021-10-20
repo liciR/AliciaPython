1424a17d3c1b432a36265a20ea721e0b8ba8764e
liciR <alicia.rodrigues@education.nsw.gov.au>
2021-10-21 09:48:00 +1200

    goodbyeworld

---
+++ 02GoodbyeWorld.py
@@ -0,0 +1,12 @@
+print(' ------------------------------------------------ ')
+print('|                                                |')
+print('|    02GoodbyeWorld                              |')
+print('|    Name : Alicia R                             |')
+print('|    Version : 01                                |')
+print('|                                                |')
+print(' ------------------------------------------------ ')
+print('Hello World')
+
+input("------> Program paused - press enter to continue")
+print('Goodbye World')
+
