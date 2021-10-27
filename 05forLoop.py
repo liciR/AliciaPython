b36dfb97490f7bb783881e65da781723ba8c5cb2
liciR <alicia.rodrigues@education.nsw.gov.au>
2021-10-28 10:38:23 +1100

    05forloop

---
+++ 05forLoop.py
@@ -0,0 +1,9 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05forLoop                                   |")
+print("|    Name : Alicia Rodrigues                     |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+for x in range(1, 500):
+  print(x)
