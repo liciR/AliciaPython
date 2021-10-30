daebce95464cd88d4829213131dcb1e6ff805b29
liciR <alicia.rodrigues@education.nsw.gov.au>
2021-10-30 14:02:44 +1100

    06whileloop

--- 06whileLoop.py
+++ 06whileLoop.py
@@ -5,15 +5,15 @@ print("|    Name : Alicia Rodrigues                     |")
 print("|    Version : 01                                |")
 print("|                                                |")
 print(" ------------------------------------------------")
-subject = input("What is the name of the subject ")
+subject = input("What is the name of this subject ")
 a = "IST"
 while subject != a:
     print("Not Correct - try again")
-    subject = input("What is the name of the subject ")
+    subject = input("What is the name of this subject ")
 else:
     print("")
     print("")
-    print("Congratulations!!")
+    print(" Congratulations!!")
     print("")
     print("")
 
